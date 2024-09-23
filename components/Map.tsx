34dccfab6231f041fadfda372d885a03bc53e547
rimvanvliet <ruud@vliet.io>
2024-09-23 11:51:40 +0200

    initial commit - working map - all markers - no subcategories - no filter - no search

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,70 +1,44 @@
 'use client'
 
 import L from 'leaflet'
-import MarkerIcon from '../node_modules/leaflet/dist/images/marker-icon.png'
-import MarkerShadow from '../node_modules/leaflet/dist/images/marker-shadow.png'
 import 'leaflet/dist/leaflet.css'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import { useState } from 'react'
+import {MapContainer, Marker, Popup, TileLayer} from 'react-leaflet'
+
+import {useState} from 'react'
+
+import {data} from "@/data/Themas"
+import MarkerClusterGroup from "react-leaflet-cluster";
 
 const Map = () => {
 
-    const [coord, setCoord] = useState([51.505, -0.09])
-
-    const SearchLocation = () => {
-        return (
-            <div className="search-location">
-                <input type="text" placeholder="Search Location" />
-            </div>
-        )
-    }
-
-    const GetMyLocation = () => {
-        const getMyLocation = () => {
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition((position) => {
-                    setCoord([position.coords.latitude, position.coords.longitude])
-                })
-            } else {
-                console.log("Geolocation is not supported by this browser.")
-            }
-        }
-
-        return (
-            <div className="get-my-location">
-                <button onClick={getMyLocation}>Get My Location</button>
-            </div>
-        )
-    }
+    const [coord, setCoord] = useState(new L.LatLng(52.258039790602496, 6.182305555985334))
+    const markers = data
 
     return (
         <div>
-            <SearchLocation />
-            <GetMyLocation />
-            <MapContainer style={{
-                height: '100vh',
-                width: '100vw'
-            }} center={coord} zoom={13} scrollWheelZoom={false}>
+            <MapContainer style={{height: '100vh', width: '100vw'}}
+                          center={coord}
+                          zoom={14}
+                          scrollWheelZoom={false}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-
-                <Marker icon={
-                    new L.Icon({
-                        iconUrl: MarkerIcon.src,
-                        iconRetinaUrl: MarkerIcon.src,
-                        iconSize: [25, 41],
-                        iconAnchor: [12.5, 41],
-                        popupAnchor: [0, -41],
-                        shadowUrl: MarkerShadow.src,
-                        shadowSize: [41, 41],
-                    })
-                } position={[51.505, -0.09]}>
-                     <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
+                <MarkerClusterGroup>
+                    {markers.map(marker =>
+                        <Marker
+                            key={marker.id}
+                            icon={
+                                new L.Icon({
+                                    iconUrl: marker.icon,
+                                    iconSize: [25, 25],
+                                })}
+                            position={L.latLng(marker.coordinates[1], marker.coordinates[0])}>
+                            <Popup>
+                                <div dangerouslySetInnerHTML={{ __html:  marker.name + (marker.description ? ('<br/>'+ marker.description) : ('')) }}/>
+                            </Popup>
+                        </Marker>)}
+                </MarkerClusterGroup>
             </MapContainer>
         </div>
     )
---
+++ types/leaflet.markercluster.d.ts
@@ -0,0 +1,21 @@
+// types/leaflet.markercluster.d.ts
+declare module "leaflet.markercluster" {
+    function markerClusterGroup(options?: L.MarkerClusterGroupOptions): L.MarkerClusterGroup;
+
+    namespace control {
+        function layers(baseLayers: any, overlays: any, options: any): any;
+    }
+
+    interface MarkerClusterGroup extends FeatureGroup<any> {
+        addLayer(layer: Layer): this;
+        removeLayer(layer: Layer): this;
+        clearLayers(): this;
+    }
+
+    interface MarkerClusterGroupOptions {
+        // Voeg hier indien nodig meer opties toe
+        showCoverageOnHover?: boolean;
+        zoomToBoundsOnClick?: boolean;
+        spiderfyOnMaxZoom?: boolean;
+    }
+}
